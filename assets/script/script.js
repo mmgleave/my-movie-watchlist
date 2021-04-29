015e00a9fea4a3f0aeaf212cb0e35430262b61a9
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-04-29 14:59:27 -0600

    watchlist basic functionality

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,26 @@
-// search button
+// buttons
 var searchBtn = document.getElementById("search-btn");
+var searchTabBtn = document.getElementById("search-tab");
+var watchlistsTabBtn = document.getElementById("lists-tab");
 
-// search results array (empty)
+// search results arrays 
 var searchResultsArray = [];
-
-// find results array (empty)
 var findResultsArray = [];
+
+// stored watch list
+var storedWatchlist = JSON.parse(localStorage.getItem("watchlist"));
 
 // watch list array
 var watchlist = [];
 
-// search results container
+// watched list array
+var watchedList = [];
+
+// containers
+var searchFormContainer = document.getElementById("search-form-container");
 var searchResultsContainer = document.getElementById("search-results-container");
-
-// lists container
 var listsContainer = document.getElementById("lists-container");
-
-// watchlist column
 var watchlistContainer = document.getElementById("watchlist-column");
-
-// watched column
 var watchedContainer = document.getElementById("watched-column");
 
 // search button click / submit
@@ -33,35 +34,61 @@ searchBtn.addEventListener('click', function (event) {
     fetchSearchResults(searchByTitle);
 });
 
-var addToWatch = function(event){
+// add to watch list when add button clicked
+var addToWatch = function (event) {
     var targetedID = event.target.id;
     var targetedResult = document.getElementById("title-" + targetedID);
-    console.log(targetedResult);
-
     var titleToAdd = targetedResult.textContent;
-    watchlist.push(titleToAdd);
 
-    console.log(watchlist);
+    console.log(storedWatchlist);
+
+    if(storedWatchlist === null){
+        watchlist.push(titleToAdd);
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    } else {
+        watchlist = storedWatchlist;
+        watchlist.push(titleToAdd);
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    }
 };
 
-var createWatchlist = function(){
-    for(i = 0; i < watchlist.length; i++){
+// remove from list when remove button clicked
+var removeFromWatch = function(event){
+    var targeted = event.target;
+    console.log(targeted);
+    
+}
+
+var createWatchlist = function (){
+    if(storedWatchlist === null){
+        watchlist = [];
+    } else {watchlist = storedWatchlist};
+
+    for (i = 0; i < watchlist.length; i++) {
         var watchlistItem = document.createElement("div");
+        watchlistItem.classList.add("columns");
         var watchlistItemTitle = document.createElement("h4");
         watchlistItemTitle.textContent = watchlist[i];
-        
+        watchlistItemTitle.className = "column is-three-fifths";
+
         var watchedBtn = document.createElement("button");
         watchedBtn.textContent = "Watched";
-        watchedBtn.className = "button is-primary m-2"
-        
+        watchedBtn.className = "button is-info m-2"
+        watchedBtn.addEventListener("click", addToWatched);
+
         var removeBtn = document.createElement("button");
         removeBtn.textContent = "Remove";
         removeBtn.className = "button is-danger m-2"
-        
+        removeBtn.addEventListener("click", removeFromWatch);
+
         watchlistItem.append(watchlistItemTitle, watchedBtn, removeBtn);
         watchlistContainer.append(watchlistItem);
-    }
+    };
 };
+
+var addToWatched = function(event){
+    
+}
 
 // fetch search results
 var fetchSearchResults = function (searchByTitle) {
@@ -112,12 +139,12 @@ var fetchSearchResults = function (searchByTitle) {
                         if (newSearchResultObj.posterURL === "N/A" || newSearchResultObj.posterURL === null) {
                             console.log("no poster");
                         } else {
-                            // create a new div for single result container
+                            // result container
                             var resultContainer = document.createElement("div");
                             resultContainer.classList.add("box");
                             resultContainer.id = "result-" + response.imdbID;
 
-                            // create a new div for 1st row (title and year)
+                            // 1st row (title and year)
                             var titleRow = document.createElement("div");
                             var titleContent = document.createElement("h4");
                             titleRow.classList.add("block");
@@ -125,44 +152,42 @@ var fetchSearchResults = function (searchByTitle) {
                             titleContent.textContent = newSearchResultObj.title + " (" + newSearchResultObj.year + ")";
                             titleRow.append(titleContent);
 
-                            // create a new div for 2nd row
+                            // 2nd row
                             var contentRow = document.createElement("div");
                             contentRow.classList.add("block");
 
-                            // create a new div to contain poster
+                            // poster
                             var posterCol = document.createElement("div");
                             posterCol.classList.add("block");
                             var posterImg = document.createElement("img");
                             posterImg.src = newSearchResultObj.posterURL;
                             posterCol.append(posterImg);
 
-                            // create a new div to contain details (genre, plot, director, actors)
+                            // details of search result
                             var detailsCol = document.createElement("div");
                             var detailsList = document.createElement("ul");
                             detailsList.style = "none";
                             var genreLi = document.createElement("li");
-                            genreLi.textContent = newSearchResultObj.genre;
+                            genreLi.textContent = "Genre: " + newSearchResultObj.genre;
                             var plotLi = document.createElement("li");
-                            plotLi.textContent = newSearchResultObj.plot;
+                            plotLi.textContent = "Plot: " + newSearchResultObj.plot;
                             var directorLi = document.createElement("li");
-                            directorLi.textContent = newSearchResultObj.director;
-                            // var writerLi = document.createElement("li");
-                            // writerLi.textContent = newSearchResultObj.writer;
+                            directorLi.textContent = "Director: " + newSearchResultObj.director;
                             var actorsLi = document.createElement("li");
-                            actorsLi.textContent = newSearchResultObj.actors;
+                            actorsLi.textContent = "Actors: " + newSearchResultObj.actors;
 
                             // *** add if here to only append if content is not N/A or null
 
                             detailsList.append(genreLi, plotLi, directorLi, actorsLi);
                             detailsCol.append(detailsList);
-                            
+
                             contentRow.append(posterCol, detailsCol)
 
-                            // create a new row div to contain "add to watchlist" button
+                            // add to watchlist button
                             var buttonRow = document.createElement("div");
                             var addToWatchBtn = document.createElement("button");
                             addToWatchBtn.textContent = "Add to Watchlist";
-                            addToWatchBtn.className = "add-btn button is-primary";
+                            addToWatchBtn.className = "add-btn button is-info";
                             addToWatchBtn.id = response.imdbID;
                             addToWatchBtn.addEventListener("click", addToWatch);
                             buttonRow.append(addToWatchBtn);
@@ -174,8 +199,20 @@ var fetchSearchResults = function (searchByTitle) {
                             searchResultsContainer.append(resultContainer);
                         }
                     });
-                }
-            })        
+            }
+        })
 };
 
-// call create watch list when watchlist button is clicked
+// hide lists and show search when search tab is clicked
+searchTabBtn.addEventListener("click", function(){
+    searchFormContainer.style.display = "block";
+    listsContainer.style.display = "none";
+});
+
+// hide search and show lists when lists tab it clicked
+// generate watchlist from watchlist array
+watchlistsTabBtn.addEventListener("click", function(){
+    createWatchlist();
+    searchFormContainer.style.display = "none";
+    listsContainer.style.display = "block";
+});
